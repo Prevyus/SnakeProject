adca9a7697b212eb54bafc9897614dea823ab1d3
Santiago Valera Rocha <104741481+Prevyus@users.noreply.github.com>
2025-05-03 21:00:59 +0200

    Updated game to laptop

--- SnakeGame/Intermediate/Build/Win64/UnrealEditor/Inc/SnakeGame/UHT/SnakeGame.init.gen.cpp
+++ SnakeGame/Intermediate/Build/Win64/UnrealEditor/Inc/SnakeGame/UHT/SnakeGame.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeSnakeGame_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xA84DFB1A,
+				0xB5823002,
 				0x696B7D72,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCodeSnakeGame_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_SnakeGame.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_SnakeGame(Z_Construct_UPackage__Script_SnakeGame, TEXT("/Script/SnakeGame"), Z_Registration_Info_UPackage__Script_SnakeGame, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xA84DFB1A, 0x696B7D72));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_SnakeGame(Z_Construct_UPackage__Script_SnakeGame, TEXT("/Script/SnakeGame"), Z_Registration_Info_UPackage__Script_SnakeGame, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB5823002, 0x696B7D72));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- SnakeGame/Intermediate/Build/Win64/x64/SnakeGameEditor/Development/Core/SharedPCH.Core.Cpp20.h
+++ SnakeGame/Intermediate/Build/Win64/x64/SnakeGameEditor/Development/Core/SharedPCH.Core.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/Users/SantiagoValeraRocha/Desktop/Assignments/C++/Unreal Engine Snake Assignment/SnakeProject/SnakeGame/Intermediate/Build/Win64/x64/SnakeGameEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
+#include "C:/Users/santi/OneDrive/Escritorio/Github/SnakeProject/SnakeGame/Intermediate/Build/Win64/x64/SnakeGameEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
 #include "Runtime/Core/Public/CoreSharedPCH.h"
